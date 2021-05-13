b3b84fd47c3d2405084c43ecf6d16433f347068b
zombie317 <aliya.husnutdinova@gmail.com>
2021-05-13 14:21:56 +0300

    add AdminLTE

--- backend/views/layouts/main-login.php
+++ backend/views/layouts/main-login.php
@@ -1,34 +1,27 @@
 <?php
+use backend\assets\AppAsset;
+use yii\helpers\Html;
 
 /* @var $this \yii\web\View */
 /* @var $content string */
 
-use backend\assets\AppAsset;
-use yii\helpers\Html;
-
-AppAsset::register($this);
+dmstr\web\AdminLteAsset::register($this);
 ?>
 <?php $this->beginPage() ?>
 <!DOCTYPE html>
 <html lang="<?= Yii::$app->language ?>">
 <head>
-    <meta charset="<?= Yii::$app->charset ?>">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta charset="<?= Yii::$app->charset ?>"/>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <?php $this->registerCsrfMetaTags() ?>
+    <?= Html::csrfMetaTags() ?>
     <title><?= Html::encode($this->title) ?></title>
     <?php $this->head() ?>
 </head>
-<body class="blank">
+<body class="login-page">
+
 <?php $this->beginBody() ?>
 
-<div class="container">
-
-    <div class="wrap">
-        <?= $content ?>
-    </div>
-
-</div>
+    <?= $content ?>
 
 <?php $this->endBody() ?>
 </body>
